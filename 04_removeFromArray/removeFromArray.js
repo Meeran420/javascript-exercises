62bac05be3c130d7d177e0627a6c8ffde982d3bc
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-10 13:41:35 +0300

    Add completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let indexOf = 0;
 
+    console.log(indexOf)
+    for(const arg of args) {
+        if(arr.includes(arg)) {
+            indexOf = arr.indexOf(arg);
+            console.log(indexOf);
+            arr.splice(indexOf, 1);
+            console.log(arr + "\n" + arr[indexOf]);
+        }
+    }
+
+    console.log(arr + "\n" + arr[indexOf])
+
+    return arr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
