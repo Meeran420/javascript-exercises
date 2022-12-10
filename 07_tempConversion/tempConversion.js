cf16df374c8592321eb0f4d1b6f55c099bee9997
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-10 14:09:42 +0300

    Add completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let result = (fahr - 32) * 5 / 9;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let result = cels * 9 / 5 + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
