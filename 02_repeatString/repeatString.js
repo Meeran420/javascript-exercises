9428c25ea5238453977b397e7eb50166ef26d7bc
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-10 13:05:20 +0300

    Add completed exercises 2 & 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0)
+        return 'ERROR';
+    
+    let adding = str;
+    let returned = "";
+    for(let i = 0; i < num; i++) {
+        returned = returned + adding;
+    }
 
+    return returned;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let returned = "";
+    for(let i = str.length; i >= 0; i--) {
+        returned = returned + str.charAt(i);
+    }
 
+    return returned;
 };
 
 // Do not edit below this line
